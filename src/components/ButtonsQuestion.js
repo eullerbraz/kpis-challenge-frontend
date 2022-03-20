e37c60daaf036533349ac937a265d3513f7fa130
Euller Braz <euller.braz99@gmail.com>
2022-03-20 00:40:12 -0300

    Adicionado descrição na pergunta de satisfação

--- src/components/ButtonsQuestion.js
+++ src/components/ButtonsQuestion.js
@@ -15,23 +15,33 @@ const ButtonsQuestion = ({ question }) => {
   }
 
   return (
-    <div className="input-buttons-container">
-      {
-        options.map((option, index) => {
-          const className = option === answer.satisfaction ?
-          'input-button selected-option' : 'input-button';
-
-          return (
-            <div
-              className={ className }
-              key={ index }
-              onClick={ handleClick }
-            >
-              { option }
-            </div>
-          );
-        })
-      }
+    <div className="answer-container">
+      <>
+        <section className="input-buttons-container">
+          {
+            options.map((option, index) => {
+              const className = option === answer.satisfaction ?
+              'input-button selected-option' : 'input-button';
+              return (
+                <div
+                  className={ className }
+                  key={ index }
+                  onClick={ handleClick }
+                >
+                  { option }
+                </div>
+              );
+            })
+          }
+        </section>
+        <section className="description-container">
+          <span className="optional">Opcional</span>
+          <h2 className="comment-title">Comentário</h2>
+          <textarea
+            placeholder="Caso queira, adicione os motivos da sua avaliação"
+          />
+        </section>
+      </>
     </div>
   );
 }
