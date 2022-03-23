26fadb428052e1c2329ac87c85ebc10a9b2229ca
Euller Braz <euller.braz99@gmail.com>
2022-03-23 17:09:45 -0300

    Estilizado página de agradecimento

--- src/pages/Thanks/index.js
+++ src/pages/Thanks/index.js
@@ -1,8 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 
 import Footer from '../../components/Footer';
-
 import logo from '../../images/logo.svg'
+import './index.css';
+
 
 const Thanks = () => {
   const navigate = useNavigate();
@@ -13,31 +14,33 @@ const Thanks = () => {
 
   return (
     <>
-      <main>
-        <img
-          src={logo}
-          alt="Company logo"
-          className="thanks-logo"
-        />
-        <div
-          className="welcome-text-container"
-        >
-          <span>
-            Muito obrigado por preencher o nosso formulário!
-          </span>
-          <span>
-            Sua opinião é muito importante para o constante crescimento da emmpresa
-          </span>
-          <span>
-            Gostaria de verificar nosso gráfico sobre esse formulário?
-          </span>
+      <main className='thanks-main'>
+        <div className='thanks-main-content'>
+          <img
+            src={logo}
+            alt="Company logo"
+            className="thanks-logo"
+          />
+          <div
+            className="thanks-text-container"
+          >
+            <span>
+              Muito obrigado por preencher o nosso formulário!
+            </span>
+            <span>
+              Sua opinião é muito importante para o constante crescimento da emmpresa
+            </span>
+            <span>
+              Gostaria de verificar nosso gráfico sobre esse formulário?
+            </span>
+          </div>
+          <button
+            onClick={ onClick }
+            className="button chart-button"
+          >
+            Gráficos
+          </button>
         </div>
-        <button
-          onClick={ onClick }
-          className="button chart-button"
-        >
-          Gráficos
-        </button>
       </main>
       <Footer />
     </>
