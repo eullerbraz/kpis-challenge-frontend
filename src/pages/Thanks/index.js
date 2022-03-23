09a9858f3df2f9daa4580a132d509057aa449168
Euller Braz <euller.braz99@gmail.com>
2022-03-23 18:41:55 -0300

    Remover o footer de thanks

--- src/pages/Thanks/index.js
+++ src/pages/Thanks/index.js
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 
-import Footer from '../../components/Footer';
 import logo from '../../images/logo.svg'
 import './index.css';
 
@@ -13,37 +12,34 @@ const Thanks = () => {
   }
 
   return (
-    <>
-      <main className='thanks-main'>
-        <div className='thanks-main-content'>
-          <img
-            src={logo}
-            alt="Company logo"
-            className="thanks-logo"
-          />
-          <div
-            className="thanks-text-container"
-          >
-            <span>
-              Muito obrigado por preencher o nosso formulário!
-            </span>
-            <span>
-              Sua opinião é muito importante para o constante crescimento da empresa
-            </span>
-            <span>
-              Gostaria de verificar nosso gráfico sobre esse formulário?
-            </span>
-          </div>
-          <button
-            onClick={ onClick }
-            className="button chart-button"
-          >
-            Gráficos
-          </button>
+    <main className='thanks-main'>
+      <div className='thanks-main-content'>
+        <img
+          src={logo}
+          alt="Company logo"
+          className="thanks-logo"
+        />
+        <div
+          className="thanks-text-container"
+        >
+          <span>
+            Muito obrigado por preencher o nosso formulário!
+          </span>
+          <span>
+            Sua opinião é muito importante para o constante crescimento da empresa
+          </span>
+          <span>
+            Gostaria de verificar nosso gráfico sobre esse formulário?
+          </span>
         </div>
-      </main>
-      <Footer />
-    </>
+        <button
+          onClick={ onClick }
+          className="button chart-button"
+        >
+          Gráficos
+        </button>
+      </div>
+    </main>
   );
 }
 
